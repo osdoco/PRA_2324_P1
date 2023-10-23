f95e8f3ea3ff42cb8137b6a4f2cef9aa3dd36f84
osdoco <osdoco2004@gmail.com>
2023-10-23 09:41:39 +0200

    hola de nuevo

--- List.h
+++ List.h
@@ -1,16 +1,17 @@
- #ifndef LIST_H
+#ifndef LIST_H
 #define LIST_H
 
 template <typename T> 
 class List {
     public:
-        void append(T e);
-            void prepend(T e);
-            T remove(int pos);
-            T get(int pos);
-            int search(T e);
-            bool empty();
-            int size();
+       virtual  void append(T e);
+       virtual  void prepend(T e);
+
+       virtual T remove(int pos);
+       virtual T get(int pos);
+       virtual  int search(T e);
+       virtual bool empty();
+       virtual int size();
 
 };
 #endif 
