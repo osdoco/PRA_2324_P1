43cbc0a27a118c032b8bda9ee7177c523675d219
osdoco <osdoco2004@gmail.com>
2023-11-13 22:01:44 +0100

    muchos cambios

--- bin/ListLinked.h
+++ bin/ListLinked.h
@@ -5,134 +5,144 @@
 template <typename T>
 class ListLinked : public List<T> {
 
-    private:
- 
-        Node<T>* first;
-	int n;
+	private:
 
-    public:
+    	Node<T>* first;
+    	int n;
 
-        ListLinked(){
-	first = nullptr;
-	n = 0;
-	}
+	public:
 
-	~ListLinked(){
-	Node<T>* aux = first;
-	for(int i = 0; i <= n; i++){
-	aux = first->next;
-	delete[] first;
-	first = aux;
-	 }
-	}
+    	ListLinked(){
+	  first = new Node(0);
+    	n = 0;
+    	}
 
-	T operator[](int pos){
-	if(pos < 0 || pos > n){
-		throw std::out_of_range("posición no válida");
-	 }
-	else{
-		
-	 	for(int i = 0; first != nullptr && i != pos; i++){
-		first = first->next;
-		}
-		return first;
-	 }
+    	~ListLinked(){
+    	Node<T>* aux = first;
+    	for(int i = 0; i <= n; i++){
+    	aux = first;
+    	first = first->next;
+    	delete[] aux;
+     	}
+    	}
+
+    	T operator[](int pos){
+    	if(pos < 0 || pos > n){
+            	throw std::out_of_range("posición no válida");
+     	}
+    	else{
+            	Node<T>* aux;
+            	for(int i = 0; aux != nullptr && i != pos; i++){
+		  aux = aux->next;
+		  }
+            	return aux->data;
+     	}
 	}
 
  friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
-
-	 out << "[";
-	 while(list.first != nullptr){
-		out << list.first;
-		list.first = list.first->next;
-		if(list.first != nullptr){
-		out<<", ";
-		}
-	 }
-	 return out;
+    	Node<T>* aux = list.first;
+     	out << "[";
+     	while(aux != nullptr){
+            	out << aux;
+            	aux = aux->next;
+            	if(aux != nullptr){
+            	out<<", ";
+            	}
+            	out<<"]";
+     	}
+     	return out;
  }
 //OVERRIDE DE LAS FUNCIONES DE List.h------------------------------------------
 
-        void insert (int pos,T e) override{
-                if (pos < 0 || pos > n){
-                throw std::out_of_range("posición inválida");
-                }
-                else {
-                
-                for(int i = 0; i < pos - 1; i++){
-                        first = first->next;
-                }
+    	void insert (int pos,T e){
+            	if (pos < 0 || pos > n){
+            	throw std::out_of_range("posición inválida");
+            	}
+            	else {
+            	Node<T>* aux;
+            	aux = first;
+            	for(int i = 0; i < pos - 1; i++){
+                   	aux = aux->next;
+            	}
+            	Node<T>* ele = new Node(e,aux->next);
+            	aux->next = ele;
+            	n++;
+            	}
+     	}
+     	void append(T e) override{
+            	Node<T>* aux;
+            	aux = first;
+            	while(first->next != nullptr){
+            	aux = aux->next;
+            	}
+		Node<T>* ele = new Node(e);
+            	aux->next = ele;//ahora la última apunta a e, e ya apunta a nullptr ya que es el último
+            	n++;
+	}
+
+     	void prepend(T e) override{
+            	Node<T>* aux;
+            	aux = first;
+		Node<T>* ele = new Node(e,aux);
+            	first = ele;
+            	n++;
+            	}
+     	T remove (int pos) override {
+            	if (pos < 0 || pos > n){
+                   	throw std::out_of_range("posición inválida");
+                	}
+            	else {
+            	Node<T>* aux;
+            	Node<T>* aux1;
+		Node<T>* val;
+            	aux = first;
+            	for(int i = 0; i <= pos-1; i++){
+		  aux = aux->next;
+            	}
+		val = aux->next;
+            	aux1=aux->next;
+            	aux->next = aux1->next;
+            	n--;
+		return val->data;
+            	}
+		
+	}
+        T get(int pos)override{
+            	if (pos < 0 || pos > n){
+                    	throw std::out_of_range("posición inválida");
+              	}
+            	else {
+            	Node<T>* aux;
+              	for(int i = 0; first != nullptr && i != pos; i++){
+                    	aux = aux->next;
+              	}
+                     	return aux->data;
+            	}
+
+	}
+
+
+    	int search(T e)override{
+            	int pos = 0;
 		Node<T>* aux;
-		aux = first->next;
-                first->next = e;
-		first = first->next;//aqui estamos en e
-		first->next = aux;
-                n++;
-                }
-         }
-         void append(T e) override{
-                while(first->next != nullptr){
-		first = first->next;
-		}
-		first->next = e;//ahora la última apunta a e;
-		first = first->next;
-		first->next = nullptr;//ahora el puntero e->next apunta a null ya que es el ultimo
-                n++;
-                }
-
-         void prepend(T e) override{
-                Node<T>* aux;
 		aux = first;
-		first = e;
-		first->next = aux;
-                n++;
-                }
-	 T remove (int pos) override {
-                if (pos < 0 || pos > n){
-                       throw std::out_of_range("posición inválida");
-                    }
-                else {
-                for(int i = 0; i < pos-1; i++){
-			first = first->next;
-		}
-		first->next = nullptr;	
-		n--;
-                }
-         }
+            	while(aux->next != nullptr){
+                    	pos++;
+                    	aux =  aux->next;
+                    	if (e == aux->data){
+                    	return pos;
+                    	}
+            	}
+                    	return -1;
+     	}
 
-         T get(int pos)override{
-                if (pos < 0 || pos > n){
-                        throw std::out_of_range("posición inválida");
-                  }
-                else {
-                  for(int i = 0; first != nullptr && i != pos; i++){
-                	first = first->next;
-               	  }
-                 	 return first;
-         	}
+    	bool empty()override{
+            	return n == 0;
+            	}
 
-              }
-
-                 
-        int search(T e)override{
-		int aux = 0;
-                while(first->next != nullptr){
-			aux++;
-			first = first->next;
-                        if (e == first){
-                        return aux;
-                        }
-                }
-                        return -1;
-         }
-
-        bool empty()override{
-                return n == 0;
-                }
-
-        int size()override{
-                return n;
-                }
+    	int size()override{
+            	return n;
+            	}
 
 
 };
--- bin/Node.h
+++ bin/Node.h
@@ -5,14 +5,9 @@ class Node {
 	public:
 		T data;
 		Node<T>* next;
-	Node(T dat, Node<T>* net ){
-		data=dat;
-		if(net!=nullptr){
+	Node(T dat = nullptr, Node<T>* net = nullptr ){
+		data = dat;
 		next = net;
-		 }
-		else{
-			next = nullptr;
-		}
 	}
 	friend std::ostream&operator<<(std::ostream &out, const Node<T> &node){
 		out << node.Data;
